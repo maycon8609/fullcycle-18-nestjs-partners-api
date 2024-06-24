51ccb63b122079f1ea2d65aaa3661df688b7baec
maycon silva <maycon8609@gmail.com>
2024-06-24 18:34:17 -0300

    feat: add auth module

--- apps/partner2/src/events/events.controller.ts
+++ apps/partner2/src/events/events.controller.ts
@@ -7,11 +7,13 @@ import {
   Param,
   Delete,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { EventsCoreService } from '@app/core/events/events-core.service';
 import { CreateEventRequest } from './request/create-event.request';
 import { UpdateEventRequest } from './request/update-event.request';
 import { ReserveSpotRequest } from './request/reserve-spot.request';
+import { AuthGuard } from '@app/core/auth/auth.guard';
 
 @Controller('events')
 export class EventsController {
@@ -46,6 +48,7 @@ export class EventsController {
     return this.eventsCoreService.remove(eventId);
   }
 
+  @UseGuards(AuthGuard)
   @Post(':eventId/reserve')
   reserveSpots(
     @Body() reserveSpotRequest: ReserveSpotRequest,
